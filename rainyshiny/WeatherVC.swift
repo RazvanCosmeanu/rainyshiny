f62fd36e70a165ba0b47c6c0b2a31f29f21451bb
hey <razvan.cosmeanu@gmail.com>
2017-07-18 15:10:31 +0300

    [NEW] initial UI setup

---
+++ rainyshiny/WeatherVC.swift
@@ -0,0 +1,26 @@
+//
+//  WeatherVC.swift
+//  rainyshiny
+//
+//  Created by hey on 18/07/2017.
+//  Copyright © 2017 hey. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var temperatureLbl: UILabel!
+    @IBOutlet weak var currentWeatherImg: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
